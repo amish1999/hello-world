f35cacd1650c0dd736b678d52e2d36397be1c9ea
amish1999 <47895005+amish1999@users.noreply.github.com>
2019-10-01 00:22:53 +0530

    Add files via upload

---
+++ xor_update.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <map>
+#include <vector>
+    using namespace std;
+ 
+    int main()
+    {
+    	int T;
+    	cin>>T;
+    	while(T--)
+    	{
+    		int N;
+    		cin>>N;
+    		int tab[N],x=0;
+    		for (int i = 0; i < N; ++i)
+    		{
+    			cin>>tab[i];
+    		}
+    		int i=0;
+    		int j=0,r=0;
+    		while(i<N)
+    		{
+    			x^=tab[i];
+    			if(x==0)
+    			{
+    				r+=i-j;
+    				j++;
+    				i=j+1;
+ 
+    				x=tab[j];
+    			}
+    			else
+    			{	
+    				i++;				
+    			}
+    		}
+ 
+    		cout<<r<<endl;
+    	}
+    	return 0;
+    }
